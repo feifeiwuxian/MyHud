13fde781a89f4341829d49877e16d26b887fa694
王飞 <>
2018-02-26 17:32:25 +0800

    重新建文件
    Signed-off-by: 王飞 <>

---
+++ MyHud/Classes/MyHud.swift
@@ -0,0 +1,15 @@
+//
+//  MyHud.swift
+//  YiTuo_Swift
+//
+//  Created by 王飞 on 2018/2/26.
+//  Copyright © 2018年 renywell. All rights reserved.
+//
+
+import UIKit
+
+class MyHud: NSObject {
+    func hello(str: String){
+        print(str)
+    }
+}
