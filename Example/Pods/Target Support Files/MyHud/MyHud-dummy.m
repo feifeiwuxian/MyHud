c2ea8c0663cf8603a59d371b8a5833af769dd207
王飞 <>
2018-02-26 16:50:59 +0800

    初始化提交Myhud Lib
    Signed-off-by: 王飞 <>

---
+++ Example/Pods/Target Support Files/MyHud/MyHud-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MyHud : NSObject
+@end
+@implementation PodsDummy_MyHud
+@end
---
+++ Example/Pods/Target Support Files/Pods-MyHud_Tests/Pods-MyHud_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyHud_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_MyHud_Tests
+@end
